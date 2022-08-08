806acaf496831e36e384b2e9514dd310abb81517
Luis Castillo <lcastillov123@gmail.com>
2022-08-08 09:19:31 -0500

    corrección

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -23,7 +23,7 @@ const ProjectCard: FC<Props> = ({
 	if (layout === 0) {
 		return (
 			<>
-				<div className="col-span-10 md:col-span-5 col-start-2 mt-16 flex w-full md:w-[96%] flex-col items-start justify-center">
+				<div className="col-span-10 md:col-span-5 md:col-start-2 col-start-2 mt-16 flex w-full md:w-[96%] flex-col items-start justify-center">
 					<h1 className="font-bold text-morado">{title}</h1>
 					<p className=" font-light mt-3">{subtitle}</p>
 					<p className="font-bold mt-3 mb-6">{roles}</p>
@@ -37,7 +37,7 @@ const ProjectCard: FC<Props> = ({
 	} else {
 		return (
 			<>
-				<div className="col-span-10 md:col-span-5 col-start-2 mt-16 w-full md:w-[96%] hidden md:block">
+				<div className="col-span-10 md:col-start-2 md:col-span-5 col-start-2 mt-16 w-full md:w-[96%] hidden md:block">
 					<Image src={src} quality={100} className="w-full" />
 				</div>
 				<div className="col-span-10 md:col-span-5 col-start-2 md:col-start-7 mt-16 flex flex-col w-full md:w-[96%] items-start justify-center justify-self-end">
